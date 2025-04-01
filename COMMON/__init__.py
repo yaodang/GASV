e55e5165d9383bad9aeb5a0cea12f1760febce16
yaodang <50345585+yaodang@users.noreply.github.com>
2025-04-01 16:16:41 +0800

    Delete COMMON directory

--- COMMON/__init__.py
+++
@@ -1,6 +0,0 @@
-from COMMON.class_all import *
-from COMMON.coordinate_transfer import *
-from COMMON.time_transfer import *
-from COMMON.constant import *
-from COMMON.mathComm import *
-from COMMON.other import *
