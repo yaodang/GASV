16d461b8d57af680f9863e3475ae0fca70ab52a8
yaodang <50345585+yaodang@users.noreply.github.com>
2025-04-30 14:20:27 +0800

    Update GASV_GUI.py
    change to pyqt5

--- GASV_GUI.py
+++ GASV_GUI.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-from PySide2.QtWidgets import *
-from PySide2.QtCore import *
-from PySide2.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import pyqtSlot as Slot
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 import sys,datetime,os
 import numpy as np
 from itertools import *
